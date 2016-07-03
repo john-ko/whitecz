58bcd22087e9812e8e7efeaf2ef4c1ab103375d7
John Ko <johnko426@gmail.com>
2016-07-02 22:58:53 -0700

    start from scratch

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+/**
+ * our javascript!
+ */
+
+console.log("HELLO WORLD!");
+
+
+//http://stackoverflow.com/questions/20276166/how-do-i-make-my-navbar-change-css-class-upon-scrolling-past-an-anchor-point
